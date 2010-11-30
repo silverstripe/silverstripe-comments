c34c228ee283f7e0c54ae2ac7835ba81efcbd0df
Will Rossiter <will.rossiter@gmail.com>
2010-11-30 13:32:05 +1300

    BUGFIX: Added default _config.php for the module

---
+++ _config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Comments Default Configuration
+ *
+ * To enable comments on your own {@link DataObject}'s then
+ * add the extension 'CommentsExtension' to your object
+ *
+ * Example: mysite/_config.php
+ *
+ * <code>
+ *	Object::add_extension('Page', 'CommentsExtension');
+ * </code>
+ */
+
+Object::add_extension('Page', 'CommentsExtension');
