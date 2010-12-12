dfd085814c2643060dfd7da43d1829cc21900f5c
Will Rossiter <will.rossiter@gmail.com>
2010-12-13 12:54:14 +1300

    MINOR: added lang files

---
+++ lang/en_US.php
@@ -0,0 +1,99 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['Comment']['ALLOWCOMMENTS'] = 'Allow Comments';
+$lang['en_US']['Comment']['COMMENT'] = 'Comment';
+$lang['en_US']['Comment']['ISSPAM'] = 'Spam?';
+$lang['en_US']['Comment']['NAME'] = 'Author Name';
+$lang['en_US']['Comment']['NEEDSMODERATION'] = 'Needs Moderation?';
+$lang['en_US']['Comment']['PLURALNAME'] = array(
+	'Comments',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['Comment']['SINGULARNAME'] = array(
+	'Comment',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['CommentAdmin']['ACCEPT'] = 'Accept';
+$lang['en_US']['CommentAdmin']['APPROVED'] = 'Accepted %s comments.';
+$lang['en_US']['CommentAdmin']['APPROVEDCOMMENTS'] = 'Approved Comments';
+$lang['en_US']['CommentAdmin']['AUTHOR'] = 'Author';
+$lang['en_US']['CommentAdmin']['COMMENT'] = 'Comment';
+$lang['en_US']['CommentAdmin']['COMMENTERURL'] = 'URL';
+$lang['en_US']['CommentAdmin']['COMMENTSAWAITINGMODERATION'] = 'Comments Awaiting Moderation';
+$lang['en_US']['CommentAdmin']['DATEPOSTED'] = 'Date Posted';
+$lang['en_US']['CommentAdmin']['DELETE'] = 'Delete';
+$lang['en_US']['CommentAdmin']['DELETEALL'] = 'Delete All';
+$lang['en_US']['CommentAdmin']['DELETED'] = 'Deleted %s comments.';
+$lang['en_US']['CommentAdmin']['MARKASNOTSPAM'] = 'Mark as not spam';
+$lang['en_US']['CommentAdmin']['MARKEDNOTSPAM'] = 'Marked %s comments as not spam.';
+$lang['en_US']['CommentAdmin']['MARKEDSPAM'] = 'Marked %s comments as spam.';
+$lang['en_US']['CommentAdmin']['NAME'] = 'Name';
+$lang['en_US']['CommentAdmin']['PAGE'] = 'Page';
+$lang['en_US']['CommentAdmin']['SPAM'] = 'Spam';
+$lang['en_US']['CommentAdmin']['SPAMMARKED'] = 'Mark as spam';
+$lang['en_US']['CommentAdmin']['URL'] = 'URL';
+$lang['en_US']['CommentAdmin_SiteTree.ss']['APPROVED'] = 'Approved';
+$lang['en_US']['CommentAdmin_SiteTree.ss']['AWAITMODERATION'] = 'Awaiting Moderation';
+$lang['en_US']['CommentAdmin_SiteTree.ss']['SPAM'] = 'Spam';
+$lang['en_US']['CommentAdmin_left.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['CommentInterface']['POST'] = 'Post';
+$lang['en_US']['CommentInterface']['YOURNAME'] = 'Your name';
+$lang['en_US']['CommentInterface_Form']['AWAITINGMODERATION'] = 'Your comment has been submitted and is now awaiting moderation.';
+$lang['en_US']['CommentTableField']['FILTER'] = 'Filter';
+$lang['en_US']['CommentTableField']['SEARCH'] = 'Search';
+$lang['en_US']['CommentTableField']['SELECTALL'] = 'All';
+$lang['en_US']['CommentTableField']['SELECTNONE'] = 'None';
+$lang['en_US']['CommentTableField.ss']['APPROVE'] = 'approve';
+$lang['en_US']['CommentTableField.ss']['APPROVECOMMENT'] = 'Approve this comment';
+$lang['en_US']['CommentTableField.ss']['DELETE'] = 'delete';
+$lang['en_US']['CommentTableField.ss']['DELETEROW'] = 'Delete this row';
+$lang['en_US']['CommentTableField.ss']['EDIT'] = 'edit';
+$lang['en_US']['CommentTableField.ss']['HAM'] = 'ham';
+$lang['en_US']['CommentTableField.ss']['MARKASSPAM'] = 'Mark this comment as spam';
+$lang['en_US']['CommentTableField.ss']['MARKNOSPAM'] = 'Mark this comment as not spam';
+$lang['en_US']['CommentTableField.ss']['NOITEMSFOUND'] = 'No items found';
+$lang['en_US']['CommentTableField.ss']['SPAM'] = 'spam';
+$lang['en_US']['CommentingController']['COMMENTS'] = 'Comments';
+$lang['en_US']['CommentingController']['EMAILADDRESS'] = 'Your email address (will not be published)';
+$lang['en_US']['CommentingController']['MSGYOUPOSTED'] = 'The message you posted was:';
+$lang['en_US']['CommentingController']['PERMISSIONFAILURE'] = 'You\'re not able to post comments to this page. Please ensure you are logged in and have an appropriate permission level.';
+$lang['en_US']['CommentingController']['SPAMDETECTED'] = 'Spam detected!!';
+$lang['en_US']['CommentingController']['WEBSITEURL'] = 'Your website URL';
+$lang['en_US']['CommentsInterface.ss']['COMMENTLOGINERROR'] = 'You cannot post comments until you have logged in';
+$lang['en_US']['CommentsInterface.ss']['COMMENTPERMISSIONERROR'] = 'and that you have an appropriate permission level';
+$lang['en_US']['CommentsInterface.ss']['COMMENTPOSTLOGIN'] = 'Login Here';
+$lang['en_US']['CommentsInterface.ss']['COMMENTS'] = 'Comments';
+$lang['en_US']['CommentsInterface.ss']['COMMENTSDISABLED'] = 'Posting comments has been disabled';
+$lang['en_US']['CommentsInterface.ss']['LOGINTOPOSTCOMMENT'] = 'Login to post a comment';
+$lang['en_US']['CommentsInterface.ss']['NEXT'] = 'next';
+$lang['en_US']['CommentsInterface.ss']['NOCOMMENTSYET'] = 'No one has commented on this page yet.';
+$lang['en_US']['CommentsInterface.ss']['POSTCOM'] = 'Post your comment';
+$lang['en_US']['CommentsInterface.ss']['PREV'] = 'previous';
+$lang['en_US']['CommentsInterface.ss']['RSSFEEDALLCOMMENTS'] = 'RSS feed for all comments';
+$lang['en_US']['CommentsInterface.ss']['RSSFEEDCOMMENTS'] = 'RSS feed for comments on this page';
+$lang['en_US']['CommentsInterface.ss']['RSSVIEWALLCOMMENTS'] = 'View all Comments';
+$lang['en_US']['CommentsInterface_singlecomment.ss']['APPROVE'] = 'approve this comment';
+$lang['en_US']['CommentsInterface_singlecomment.ss']['ISNTSPAM'] = 'this comment is not spam';
+$lang['en_US']['CommentsInterface_singlecomment.ss']['ISSPAM'] = 'this comment is spam';
+$lang['en_US']['CommentsInterface_singlecomment.ss']['PBY'] = 'Posted by';
+$lang['en_US']['CommentsInterface_singlecomment.ss']['REMCOM'] = 'remove this comment';
+$lang['en_US']['PageComment']['COMMENTBY'] = array(
+	'Comment by \'%s\' on %s',
+	PR_MEDIUM,
+	'Name, Page Title'
+);
+$lang['en_US']['PageCommentInterface']['DELETEALLCOMMENTS'] = 'Delete all comments on this page';
+$lang['en_US']['TableListField']['SELECT'] = 'Select:';
+$lang['en_US']['TableListField_PageControls.ss']['DISPLAYING'] = 'Displaying';
+$lang['en_US']['TableListField_PageControls.ss']['OF'] = 'of';
+$lang['en_US']['TableListField_PageControls.ss']['TO'] = 'to';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWFIRST'] = 'View first';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWLAST'] = 'View last';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWNEXT'] = 'View next';
+$lang['en_US']['TableListField_PageControls.ss']['VIEWPREVIOUS'] = 'View previous';
+
+?>
