7f1005ed06a1a66e5438b15a20d8ad9617a007e5
Damian Mooyman <damian@silverstripe.com>
2015-01-12 14:00:20 +1300

    Provide better temporary location for HTMLPurifier cache files

--- code/dataobjects/Comment.php
+++ code/dataobjects/Comment.php
@@ -398,6 +398,7 @@ class Comment extends DataObject {
 		$config->set('AutoFormat.AutoParagraph', true);
 		$config->set('AutoFormat.Linkify', true);
 		$config->set('URI.DisableExternalResources', true);
+		$config->set('Cache.SerializerPath', getTempFolder());
 		return new HTMLPurifier($config);
 	}
 
