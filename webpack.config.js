2fc7e7a0045aca90326299bb7576d77983e26db0
Will Rossiter <will@fullscreen.io>
2017-09-14 10:01:09 +1200

    Add phpunit, lint fixes and removed compass for webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const Path = require('path');
+const webpackConfig = require('@silverstripe/webpack-config');
+const {
+  resolveJS,
+  externalJS,
+  moduleJS,
+  pluginJS,
+  moduleCSS,
+  pluginCSS,
+} = webpackConfig;
+
+const ENV = process.env.NODE_ENV;
+const PATHS = {
+  MODULES: 'node_modules',
+  ROOT: Path.resolve(),
+  FILES_PATH: '../',
+  SRC: Path.resolve(),
+};
+
+const config = [
+  {
+    name: 'css',
+    entry: {
+      bundle: Path.resolve('scss') + '/comments.scss',
+    },
+    output: {
+      path: Path.resolve('css'),
+      filename: 'styles/[name].css',
+    },
+    devtool: (ENV !== 'production') ? 'source-map' : '',
+    module: moduleCSS(ENV, PATHS),
+    plugins: pluginCSS(ENV, PATHS),
+  }
+];
+
+// Use WEBPACK_CHILD=js or WEBPACK_CHILD=css env var to run a single config
+module.exports = (process.env.WEBPACK_CHILD)
+  ? config.find((entry) => entry.name === process.env.WEBPACK_CHILD)
+  : module.exports = config;
