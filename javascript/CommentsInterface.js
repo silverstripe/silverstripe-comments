3812057b0075cf37fccc5111fa35b5a754aa6031
Dylan Wagstaff <dylan@silverstripe.com>
2019-01-11 14:37:24 +1300

    Add ability for commenters to enter a URL without a protocol
    When posting a comment on the a page with this module applied, there is
    an optional input for the commenter to give their URL,presumably their
    website. However this input currently validates (via JavaScript) to
    allow URLs only iff they have a protocol.
    
    Common use cases when someone is asked for their website in my
    experience is to then receive a URL without a protocol, confounded in
    that most web browsers will accept this form and automatically add the
    http protocol, where a webserver may then redirect to https by default.
    This means that all the magic happens behind the scenes and most folks
    don't particularly care to think about protocols when entering web
    addresses.
    
    Yest this input will only validate true and allow comment submission if
    they do.
    
    So now it will allow a protocol-less entry into the URL box.

--- javascript/CommentsInterface.js
+++ javascript/CommentsInterface.js
@@ -2,7 +2,16 @@
  * @package comments
  */
 (function($) {
+    // The above closure encapsulates the $ variable away from the global scope
+    // and the one below is the `$(document).ready(...)` shorthand.
 	$(function() {
+        // Override the default URL validator in order to extend it to allow protocol-less URLs
+		$.validator.methods.url = function( value, element ) {
+            // This line is copied directly from the jQuery.validation source (version 1.19.0)
+            // the only change is a single question mark added here ---------v
+			return this.optional( element ) || /^(?:(?:(?:https?|ftp):)?\/\/)?(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( value );
+		}
+
 		/**
 		 * Enable form validation
 		 */
